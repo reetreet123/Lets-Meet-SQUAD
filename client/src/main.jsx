ada46490818e325dcb714287146460d941997369
reetreet123 <123092335+reetreet123@users.noreply.github.com>
2024-06-06 15:41:50 +0530

    main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import {store} from "./store/store";
+import App from "./App.jsx";
+import "./index.scss";
+import { BrowserRouter  } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
